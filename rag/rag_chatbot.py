f2de4d80ad409c902cee7c43b832a79a8eb04c31
thonguyen241206 <thonguyen241206@gmail.com>
2026-03-18 22:22:47 +0700

    Update rag_chatbot.py

--- rag/rag_chatbot.py
+++ rag/rag_chatbot.py
@@ -1,31 +1,29 @@
-from rag.retriever import retrieve
-from google import genai
-import os
-from dotenv import load_dotenv
+from rag.retriever import retrieve 
+from google import genai 
+import os 
+from dotenv import load_dotenv 
 
-load_dotenv()
+load_dotenv() 
+client = genai.Client( 
+api_key=os.getenv("GEMINI_API_KEY") 
+) 
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+def ask_rag(question): 
+    contexts = retrieve(question) 
+    context_text = "\n\n".join(contexts) 
+    prompt = f""" 
+    
+Use the following context to answer the question. 
+Context: 
+{context_text} 
 
-def ask_rag(question):
-    contexts = retrieve(question)
-    context_text = "\n\n".join(contexts)
+Question: 
+{question} 
 
-    prompt = f"""
-Use the following context to answer the question.
-
-Context:
-{context_text}
-
-Question:
-{question}
-
-Answer:
-"""
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
-    )
-
-    return response.text
+Answer: 
+""" 
+response = client.models.generate_content( 
+model="gemini-2.5-flash", 
+contents=prompt 
+) 
+return response.text 
