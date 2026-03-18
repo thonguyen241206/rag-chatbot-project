0ac5d08810473f3532d7d98089822d0186289952
thonguyen241206 <thonguyen241206@gmail.com>
2026-03-18 22:18:10 +0700

    Update embedder.py

--- rag/embedder.py
+++ rag/embedder.py
@@ -1,7 +1,10 @@
-from sentence_transformers import SentenceTransformer
-import numpy as np
+from sentence_transformers import SentenceTransformer 
+import numpy as np 
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2") 
 
-def embed_text(text):
-    return np.array(model.encode(text)).astype("float32")
+def embed_text(text): 
+    print("đang xử lí text-embedding") 
+    vector = model.encode(text) 
+    print("xong task embedding!") 
+    return np.array(vector).astype("float32") 
